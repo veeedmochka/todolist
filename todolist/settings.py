611a191342145f5e3b00069de7e8576d26a892df
veeedmochka <veeedmochka@mail.ru>
2022-07-24 02:56:59 +0300

    Installed REST framework.

--- todolist/settings.py
+++ todolist/settings.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+\from pathlib import Path
 import environ
 import os
 
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
     'listapp.apps.ListappConfig',
 ]
 
