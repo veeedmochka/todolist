53141e4f5f3da53e8404bc3a40b61c73549ef6f2
veeedmochka <veeedmochka@github.com>
2022-07-27 17:59:11 +0300

    Fixed bugs in the TodoList model.

--- listapp/models.py
+++ listapp/models.py
@@ -3,8 +3,8 @@ from django.conf import settings
 
 
 class TodoList(models.Model):
-	name = models.CharField(max_length=100, verbose_name='Название', default='Новый список')
-	tasks = models.JSONField(default=dict(tasks = []), verbose_name='Список задач')
+	name = models.CharField(max_length=20, verbose_name='Название', default='Новый список')
+	tasks = models.JSONField(default=dict(tasks = {}), verbose_name='Список задач')
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Пользователь')
 
 	class Meta:
@@ -12,5 +12,4 @@ class TodoList(models.Model):
 		verbose_name_plural = 'Списки задач'
 
 	def __str__(self):
-		return f"{name} ({self.user.username}'s list)"
-
+		return f"{self.name} ({self.user.username}'s list)"
