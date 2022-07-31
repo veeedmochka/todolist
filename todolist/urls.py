f43b0ee00dde424684166a80465b565f548eb16c
veeedmochka <veeedmochka@mail.ru>
2022-07-31 18:53:25 +0300

    Updated urls.

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('listapp.urls')),
+    path('account/', include('account.urls')),
     path('api/v1/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
