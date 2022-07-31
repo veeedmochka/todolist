a6c10ea864d91d017138a90c87f02d3181bd038e
veeedmochka <veeedmochka@mail.ru>
2022-07-31 18:56:40 +0300

    Updated 'home' view.

--- listapp/views.py
+++ listapp/views.py
@@ -1,15 +1,23 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import TodoList
 
 
-class TodoListView(TemplateView):
+class TodoListView(LoginRequiredMixin, TemplateView):
 	template_name = 'listapp/home.html'
 
 	def get_context_data(self, *args, **kwargs):
 		context = super().get_context_data(*args, **kwargs)
 
-		last_list_id = self.request.COOKIES.get('last_list_id', None)
+		# получаем id последнего открывавшегося списка из cookie
+		last_list_id = str(self.request.COOKIES.get('last_list_id', None))
+		# получаем максимальную длину имени списка
+		name_max_length = TodoList._meta.get_field('name').max_length
+
 		context.update({
-			'last_list_id': last_list_id
+			'last_list_id': last_list_id,
+			'name_max_length': name_max_length
 		})
 
 		return context
