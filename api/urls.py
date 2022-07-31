806a6bd466c2e70a0b33cadb3f4f042d8937f150
veeedmochka <veeedmochka@mail.ru>
2022-07-31 18:53:14 +0300

    Updated API urls.

--- api/urls.py
+++ api/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
 	path('list/get-ids/', GetTodoLists.as_view(), name='api-get-lists'),
 	path('list/<int:pk>/delete-task/', DeleteTask.as_view(), name='api-delete-task'),
 	path('list/<int:pk>/add-task/', AddTask.as_view(), name='api-add-task'),
+	path('list/<int:pk>/update-task/', UpdateTask.as_view(), name='api-update-task'),
+	path('account/check-username/', CheckUsername.as_view(), name='api-check-username'),
+	path('account/check-password/', CheckPassword.as_view(), name='api-check-password'),
+	path('account/check-email/', CheckEmail.as_view(), name='api-check-email'),
 ]
